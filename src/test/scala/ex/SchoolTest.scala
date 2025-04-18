4f64b9634dcac2fd174390547f42025a3d8195a2
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-04-18 16:43:06 +0200

    Ex 4: Fix import integration problem with Sequences and OOP

--- src/test/scala/ex/SchoolTest.scala
+++ src/test/scala/ex/SchoolTest.scala
@@ -1,5 +1,9 @@
 package ex
 
+import ex.SchoolModel.*
+import util.Sequences.*
+import Sequence.*
+
 import org.junit.*
 import org.junit.Assert.*
 
@@ -16,38 +20,38 @@ class SchoolTest:
     assertEquals("Math", course("Math"))
 
   @Test def testEmptySchool(): Unit =
-    assertEquals(nil(), emptySchool)
+    assertEquals(Nil(), emptySchool)
 
   @Test def testGetCourses(): Unit =
-    val noCourses = emptySchool.courses()
+    val noCourses = emptySchool.courses
     val oneCourse = emptySchool
-      .setTeacherToCourse(teacher("John"), course("Math")).courses()
+      .setTeacherToCourse(teacher("John"), course("Math")).courses
     val twoCourses = emptySchool
       .setTeacherToCourse(teacher("John"), course("Math"))
-      .setTeacherToCourse(teacher("John"), course("Italian")).courses()
+      .setTeacherToCourse(teacher("John"), course("Italian")).courses
     val duplicatedCourses = emptySchool
       .setTeacherToCourse(teacher("John"), course("Math"))
-      .setTeacherToCourse(teacher("Peter"), course("Math")).courses()
-    assertEquals(nil(), noCourses)
-    assertEquals(cons("Math", nil()), oneCourse)
-    assertEquals(cons("Math", nil()), duplicatedCourses)
+      .setTeacherToCourse(teacher("Peter"), course("Math")).courses
+    assertEquals(Nil(), noCourses)
+    assertEquals(Cons("Math", Nil()), oneCourse)
+    assertEquals(Cons("Math", Nil()), duplicatedCourses)
 
   @Test def testGetTeachers(): Unit =
-    val noTeachers = emptySchool.teachers()
+    val noTeachers = emptySchool.teachers
     val oneTeacher = emptySchool
-      .setTeacherToCourse(teacher("John"), course("Math")).teachers()
+      .setTeacherToCourse(teacher("John"), course("Math")).teachers
     val duplicatedTeacher = emptySchool
       .setTeacherToCourse(teacher("John"), course("Math"))
-      .setTeacherToCourse(teacher("John"), course("Italian")).teachers()
-    assertEquals(nil(), noTeachers)
-    assertEquals(cons("John", nil()), oneTeacher)
-    assertEquals(cons("John", nil()), duplicatedTeacher)
+      .setTeacherToCourse(teacher("John"), course("Italian")).teachers
+    assertEquals(Nil(), noTeachers)
+    assertEquals(Cons("John", Nil()), oneTeacher)
+    assertEquals(Cons("John", Nil()), duplicatedTeacher)
 
   @Test def testSetTeacherToCourse(): Unit =
     val mySchool = emptySchool
       .setTeacherToCourse(teacher("John"), course("Math"))
-    assertEquals(cons("Math", nil()), mySchool.courses())
-    assertEquals(cons("John", nil()), mySchool.teachers())
+    assertEquals(Cons("Math", Nil()), mySchool.courses)
+    assertEquals(Cons("John", Nil()), mySchool.teachers)
 
   @Test def testCoursesOfATeacher(): Unit =
     val coursesSchool1 = emptySchool
@@ -59,9 +63,9 @@ class SchoolTest:
       .setTeacherToCourse(teacher("John"), course("Math"))
       .setTeacherToCourse(teacher("John"), course("Italian"))
       .coursesOfATeacher(teacher("John"))
-    assertEquals(nil(), coursesSchool1)
-    assertEquals(cons("Math", nil()), coursesSchool2)
-    assertEquals(cons("Math", cons("Italian", nil())), coursesSchool3)
+    assertEquals(Nil(), coursesSchool1)
+    assertEquals(Cons("Math", Nil()), coursesSchool2)
+    assertEquals(Cons("Math", Cons("Italian", Nil())), coursesSchool3)
 
   @Test def testHasTeacher(): Unit =
     val school1HasJohn = emptySchool
